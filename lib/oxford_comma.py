117f1af151d67fb76d3066d12878c7025cf6f5f6
Jim Grimes <jimgrimes86@gmail.com>
2023-10-23 12:17:00 -0400

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        last_index = len(items)-1
+        first_section_list = items[0:last_index]
+        first_section_string = ", ".join(first_section_list)
+        last_item_string = items[-1]
+        combined_strings_list = [first_section_string, last_item_string]
+        string = ", and ".join(combined_strings_list)
+    else:
+        string = " and ".join(items)
+    return string
